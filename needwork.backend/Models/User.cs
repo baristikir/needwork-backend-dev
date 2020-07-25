502c750005a81dbb81aaac030d08f459322c53df
Baris Tikir <brstkr@icloud.com>
2020-07-25 20:33:06 +0200

    Added EF Core to the project & DI

---
+++ needwork.backend/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace needwork.backend.Models
+{
+    public class User : IdentityUser
+    {
+        public User()
+        {
+        }
+    }
+}
--- needwork.backend/Startup.cs
+++ needwork.backend/Startup.cs
@@ -14,10 +14,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using needwork.backend.Data;
 
 namespace needwork.backend
 {
@@ -34,6 +36,10 @@ namespace needwork.backend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Adding database service for the custom DbContext class
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("ConnectionName")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
