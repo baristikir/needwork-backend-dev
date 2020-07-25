599761d2ff720e8e4fd567aa407b7421ab997521
Baris Tikir <brstkr@icloud.com>
2020-07-25 22:40:53 +0200

    Added AuthenticationScheme

--- needwork.backend/Data/ApplicationDbContext.cs
+++ needwork.backend/Data/ApplicationDbContext.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿//
+//  ApplicationDbContext.cs
+//  needwork
+//
+//  Created by Baris Tikir on 25.07.20.
+//  Copyright © 2020 Baris Tikir. All rights reserved.
+//
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace needwork.backend.Data
--- needwork.backend/Startup.cs
+++ needwork.backend/Startup.cs
@@ -9,16 +9,21 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using needwork.backend.Data;
 
 namespace needwork.backend
@@ -40,6 +45,29 @@ namespace needwork.backend
             // Adding database service for the custom DbContext class
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlite(Configuration.GetConnectionString("ConnectionName")));
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            // Defined JwtBearerDefaults AuthenticationScheme => Authorized API Controller need to use this scheme
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+              .AddJwtBearer(options =>
+              {
+                  options.TokenValidationParameters = new TokenValidationParameters
+                  {
+                      ValidateIssuer = true,
+                      ValidateAudience = true,
+                      ValidateLifetime = true,
+                      ValidateIssuerSigningKey = true,
+                      ValidIssuer = Configuration["JwtIssuer"],
+                      ValidAudience = Configuration["JwtAudience"],
+                      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtSecurityKey"]))
+                  };
+              });
+
+            // ClaimTypes.NameIdentifier - UserId 
+            services.Configure<IdentityOptions>(options =>
+                options.ClaimsIdentity.UserIdClaimType = ClaimTypes.NameIdentifier);
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +82,7 @@ namespace needwork.backend
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
